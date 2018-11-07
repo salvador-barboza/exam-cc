bf421a8661c177838ca14a402936afec9fb3f9d7
Jose Adame <huajuco4@gmail.com>
2018-11-07 15:13:26 -0600

    moved to own components, added new functionality to buttons

---
+++ src/ui/Exam/Components.tsx
@@ -0,0 +1,50 @@
+import styled  from 'react-emotion';
+
+export const EditButton = styled('button')((props: { color: string }) => ({
+    borderRadius: 5,
+    borderColor: 'green',
+    backgroundColor: 'transparent',
+    padding: 8,
+    fontSize: 14,
+    color: props.color,
+    cursor: 'pointer',
+    textAlign: 'center',
+    width: 438,
+    height: 40,
+  }))
+  
+  export const Background = styled('div')({
+    position: 'fixed',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'rgba(0,0,0,0.3)',
+    padding: 50,
+  });
+  
+  export const Popup = styled('div')({
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    maxWidth: 500,
+    minHeight: 300,
+    margin: '0 auto',
+    padding: 30,
+    position: 'relative',
+  });
+  
+  export const ButtonCool = styled('button')({
+    borderColor: '#6100ED',
+    borderRadius: 1,
+    backgroundColor:'#E7DFFB',
+    borderTopLeftRadius: 4,
+    borderTopRightRadius: 4,
+    borderBottomLeftRadius: 4,
+    borderBottomRightRadius: 4,
+    color: '#6100ED',
+    textAlign: 'center',
+    width: 438,
+    height: 40,
+    marginBottom: 10,
+    fontSize: 17,
+  });
--- src/ui/Exam/QuestionBankPopUp.tsx
+++ src/ui/Exam/QuestionBankPopUp.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { css } from 'emotion'
-import { IQuestionBank, QuestionCount } from 'src/models/QuestionBank/IQuestionBank'
-import {EditButton, Background, Popup, ButtonCool} from 'src/ui/Editor/AnswerEditor/Components'
+import { IQuestionBank} from 'src/models/QuestionBank/IQuestionBank'
+import {Background, Popup, ButtonCool} from './Components'
 
 interface QuestionBankPopUpProps {
     show: boolean
     subjects: IQuestionBank[]
-    onClicked: (id: number[]) => void
+    onClicked: (id: string) => void
 }
 
 interface QuestionBankPopUpState {
     show: boolean
-    selected: number[]
+    selected: string[]
 }
 
 class QuestionBankPopUp extends React.Component<QuestionBankPopUpProps, QuestionBankPopUpState> {
@@ -25,45 +25,14 @@ class QuestionBankPopUp extends React.Component<QuestionBankPopUpProps, Question
         this.setState({ show: false })
     }
 
-    public setSelected(id: number) {
-        let temp = this.state.selected
-        if (!this.isInArray(id, temp)) {
-            temp.push(id)
-            this.setState({ selected: temp })
-        } else {
-            temp.splice(temp.indexOf(id), 1)
-            this.setState({ selected: temp })
-        }
-    }
-
-    private isInArray(value: number, array: number[]) {
-        return array.indexOf(value) > -1;
-    }
-
     private listsubject(subjects: IQuestionBank[]) {
-        let select = this.state.selected
         return (
             subjects.map((subject, index) =>
-                <ButtonCool onClick={() => this.setSelected(index)} selected={this.isInArray(index, select)}>{subject.title}---{
-                    this.formatQuestionBankCount(subject.questionCount!!)
+                <ButtonCool onClick={() => this.props.onClicked(this.props.subjects[index].id)}>{subject.title}---{
+                    this.props.subjects[index].questionCount
                 }</ButtonCool>
             )
         )
-    }
-
-    private formatQuestionBankCount = ({ easy, medium, hard }: QuestionCount) => {
-        const total = easy + medium + hard
-        return `${total} pregunta${total === 1 ? '' : 's'}.`
-    }
-
-    private showArray = () => {
-        if (this.state.selected!=[]) {
-            let temp = this.state.selected
-            temp.sort()
-            this.setState({ selected: temp })
-            this.props.onClicked(this.state.selected)
-        }
-        this.props.onClicked([])
     }
 
     render() {
@@ -78,7 +47,6 @@ class QuestionBankPopUp extends React.Component<QuestionBankPopUpProps, Question
                         <div className={css({ overflowY: 'scroll', height: 400 })}>
                             <button onClick={this.hide} className={css({ position: 'absolute', right: 10, top: 10 })}>Close</button>
                             {this.listsubject(this.props.subjects)}
-                            <EditButton color={'green'} onClick={this.showArray}>Confirmar</EditButton>
                         </div>
                     </div>
                 </Popup>
