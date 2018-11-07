30356debbbdf98604bea600ade64b0aac0c6a989
paez74 <paez74@live.com.mx>
2018-11-06 20:31:06 -0600

    Started QuestionBanksCountContainer

--- src/ui/Exam/QuestionBanksCountContainer.tsx
+++ src/ui/Exam/QuestionBanksCountContainer.tsx
@@ -1,3 +1,34 @@
 // recibe n QuestionBanks
 //
 //necesita regresar un arreglo con id del question bank y el numero de easy , medium, hard
+import React from 'react'
+import {IQuestionBank} from 'src/models/QuestionBank/IQuestionBank'
+interface QuestionBanksCountContainerProps{
+  questionbanks?: IQuestionBank[];
+}
+
+class QuestionBanksCountContainer extends React.Component <QuestionBanksCountContainerProps>{
+constructor(props){
+  super(props)
+}
+
+/*
+private Count = (questionbanks:IQuestionBank[]) => {
+  var obj = questionbanks.map(function(questionbank)) {
+    return({
+    id:questionbank.id,
+    easy:questionbank.easy,
+    medium:questionbank.medium,
+    hard:questionbank.medium });
+  }
+} */
+
+
+ render(){
+  return (<div></div>);
+}
+
+}
+
+
+ export default QuestionBanksCountContainer
