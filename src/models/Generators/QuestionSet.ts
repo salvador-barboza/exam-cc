71f1640124939d9774ab40ba8e8378f0f6ba9c71
FiacroReyes <fgrl1408@gmail.com>
2018-11-07 16:21:20 -0600

    QuestionSet Completed

---
+++ src/models/Generators/QuestionSet.ts
@@ -0,0 +1,36 @@
+import { IQuestion } from '../Question/IQuestion'
+
+export class QuestionSet {
+    constructor(
+        private questionBanks: Map<string, IQuestion[]>,
+        private requiredQuestions: Map<string, number>
+    ) { }
+
+    result: IQuestion[] = [];
+
+    public commpute = () => {
+        [...this.questionBanks].forEach(([key, value]) => {
+
+            if (this.requiredQuestions.get(key) != undefined) {
+                const usedQuestion: boolean[] = [];
+
+                for (let i = 0; i < value.length; i++) {
+                    usedQuestion.push(false);
+                }
+
+                for (let i = 0; i < this.requiredQuestions.get(key)!; i++) {
+                    let generatedIndex = Math.floor(Math.random() * value.length);
+                   
+                    while (usedQuestion[generatedIndex]) {
+                        generatedIndex = Math.floor(Math.random() * value.length);
+                    }
+                    
+                    usedQuestion[generatedIndex] = true;
+                    this.result.push(value[generatedIndex]);
+                }
+            }
+        })
+
+        return this.result;
+    }
+}
