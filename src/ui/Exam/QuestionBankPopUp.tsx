ca38bc663c8e4618fed18c38ab14abff60ed4edd
Jose Adame <huajuco4@gmail.com>
2018-11-07 10:55:10 -0600

    beautifyed

--- src/ui/Exam/QuestionBankPopUp.tsx
+++ src/ui/Exam/QuestionBankPopUp.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import { css } from 'emotion'
 import styled from 'react-emotion'
-import {IQuestionBank,QuestionCount} from 'src/models/QuestionBank/IQuestionBank'
+import { IQuestionBank, QuestionCount } from 'src/models/QuestionBank/IQuestionBank'
 
-interface QuestionBankPopUpProps{
+interface QuestionBankPopUpProps {
     show: boolean
     subjects: IQuestionBank[]
 }
 
-interface QuestionBankPopUpState{
+interface QuestionBankPopUpState {
     show: boolean
-    selected:number[]
+    selected: number[]
 }
 
 const EditButton = styled('button')((props: { color: string }) => ({
-    borderRadius:5,
-    borderColor:'green',
+    borderRadius: 5,
+    borderColor: 'green',
     backgroundColor: 'transparent',
     padding: 8,
     fontSize: 14,
@@ -24,7 +24,7 @@ const EditButton = styled('button')((props: { color: string }) => ({
     textAlign: 'center',
     width: 438,
     height: 40,
-  }))
+}))
 
 const Background = styled('div')({
     position: 'fixed',
@@ -34,109 +34,109 @@ const Background = styled('div')({
     right: 0,
     backgroundColor: 'rgba(0,0,0,0.3)',
     padding: 50,
-    });
+});
 
-    const Popup = styled('div')({
-        backgroundColor: '#fff',
-        borderRadius: 5,
-        maxWidth: 500,
-        minHeight: 300,
-        margin: '0 auto',
-        padding: 30,
-        position: 'relative',
-    });
+const Popup = styled('div')({
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    maxWidth: 500,
+    minHeight: 300,
+    margin: '0 auto',
+    padding: 30,
+    position: 'relative',
+});
 
-    interface ButonProp{
-        selected: boolean
-    }
+interface ButonProp {
+    selected: boolean
+}
 
-    const ButtonCool = styled('button')((state: ButonProp) =>({
-        borderColor: '#6100ED',
-        borderRadius: 1,
-        backgroundColor: state.selected? '#E7DFFB': '#fff',
-        borderTopLeftRadius: 4,
-        borderTopRightRadius: 4,
-        borderBottomLeftRadius: 4,
-        borderBottomRightRadius: 4,
-        color: '#6100ED',
-        textAlign: 'center',
-        width: 438,
-        height: 40,
-        marginBottom: 10,
-        fontSize: 17,
-    }));
+const ButtonCool = styled('button')((state: ButonProp) => ({
+    borderColor: '#6100ED',
+    borderRadius: 1,
+    backgroundColor: state.selected ? '#E7DFFB' : '#fff',
+    borderTopLeftRadius: 4,
+    borderTopRightRadius: 4,
+    borderBottomLeftRadius: 4,
+    borderBottomRightRadius: 4,
+    color: '#6100ED',
+    textAlign: 'center',
+    width: 438,
+    height: 40,
+    marginBottom: 10,
+    fontSize: 17,
+}));
 
-class QuestionBankPopUp extends React.Component <QuestionBankPopUpProps,QuestionBankPopUpState> {
-    public state: QuestionBankPopUpState 
-    constructor(props){
+class QuestionBankPopUp extends React.Component<QuestionBankPopUpProps, QuestionBankPopUpState> {
+    public state: QuestionBankPopUpState
+    constructor(props) {
         super(props)
-        this.state = ({show: props.show,selected:[]})
+        this.state = ({ show: props.show, selected: [] })
     }
 
     public hide = () => {
-        this.setState({show:false})
+        this.setState({ show: false })
     }
 
-    public setSelected(id: number){
+    public setSelected(id: number) {
         let temp = this.state.selected
-        if(!this.isInArray(id,temp)){
-        temp.push(id)
-        this.setState({selected:temp})
-        console.log(this.state.selected)
-        }else{
-        temp.splice(temp.indexOf(id),1)
-        this.setState({selected:temp})
-        console.log(this.state.selected)
+        if (!this.isInArray(id, temp)) {
+            temp.push(id)
+            this.setState({ selected: temp })
+            console.log(this.state.selected)
+        } else {
+            temp.splice(temp.indexOf(id), 1)
+            this.setState({ selected: temp })
+            console.log(this.state.selected)
         }
     }
 
-    private isInArray(value:number, array:number[]) {
+    private isInArray(value: number, array: number[]) {
         return array.indexOf(value) > -1;
-      }
-      
-    private listsubject(subjects: IQuestionBank[]){
+    }
+
+    private listsubject(subjects: IQuestionBank[]) {
         let select = this.state.selected
-        return(
-        subjects.map((subject,index) => 
-        <ButtonCool onClick={()=>this.setSelected(index)} selected={this.isInArray(index,select)}>{subject.title}---{
-            this.formatQuestionBankCount(subject.questionCount!!)
-        }</ButtonCool>
-        )
+        return (
+            subjects.map((subject, index) =>
+                <ButtonCool onClick={() => this.setSelected(index)} selected={this.isInArray(index, select)}>{subject.title}---{
+                    this.formatQuestionBankCount(subject.questionCount!!)
+                }</ButtonCool>
+            )
         )
     }
 
-    private formatQuestionBankCount = ({ easy, medium, hard}: QuestionCount) => {
+    private formatQuestionBankCount = ({ easy, medium, hard }: QuestionCount) => {
         const total = easy + medium + hard
         return `${total} pregunta${total === 1 ? '' : 's'}.`
     }
 
     private showArray = () => {
-        if(this.state.selected){
-        let temp = this.state.selected
-        temp.sort()
-        this.setState({selected:temp})
-        console.log(this.state.selected)
+        if (this.state.selected) {
+            let temp = this.state.selected
+            temp.sort()
+            this.setState({ selected: temp })
+            console.log(this.state.selected)
         }
     }
 
-    render(){
-        if(!this.state.show) {
+    render() {
+        if (!this.state.show) {
             return null;
         }
-          return (
+        return (
             <Background>
                 <Popup>
                     {this.props.children}
                     <div className="footer">
-                    <div className={css({overflowY:'scroll', height:400})}>
-                    <button onClick={this.hide} className={css({position:'absolute', right:10,top:10})}>Close</button>
-                    {this.listsubject(this.props.subjects)}
-                    <EditButton color={'green'} onClick={this.showArray}>Confirmar</EditButton>
-                    </div>
+                        <div className={css({ overflowY: 'scroll', height: 400 })}>
+                            <button onClick={this.hide} className={css({ position: 'absolute', right: 10, top: 10 })}>Close</button>
+                            {this.listsubject(this.props.subjects)}
+                            <EditButton color={'green'} onClick={this.showArray}>Confirmar</EditButton>
+                        </div>
                     </div>
                 </Popup>
             </Background>
-          );
+        );
     }
 }
 export default QuestionBankPopUp
