f2662cb24d885cd9fcd8f489dffe12146c115a7a
Jose Adame <huajuco4@gmail.com>
2018-11-11 15:04:55 -0600

    fixed index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 /* tslint:ignore-next */
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-//import App from './App';
+import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import QuestionBankCollection from './ui/Exam/QuestionBankCollection'
+
 ReactDOM.render(
-  <QuestionBankCollection questioncollections={[{id:"01",questionCount:3,title:"suma"},{id:"02",questionCount:6,title:"resta"},{id:"03",questionCount:4,title:"multiplicacion"}]} />,
+  <App />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
