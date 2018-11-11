40b92856a7354c357989cd73cb02c87ef2390515
Salvador Barboza <salvador.barboza.portillo@gmail.com>
2018-11-11 17:11:30 -0600

    Add exam generator (unstyled)

--- src/ui/Exam/QuestionBankPopUp.tsx
+++ src/ui/Exam/QuestionBankPopUp.tsx
@@ -1,84 +1,57 @@
 import React from 'react'
 import { css } from 'emotion'
 import { IQuestionBank } from 'src/models/QuestionBank/IQuestionBank'
-import {EditButton, Background, Popup, ButtonCool} from './Components'
+import { EditButton, Background, Popup, ButtonCool } from './Components'
 
 interface QuestionBankPopUpProps {
-    show: boolean
     subjects: IQuestionBank[]
-    onClicked: (id: string[]) => void
-    onHide: () => void
+    onSubmit: (id: string[]) => void
 }
 
 interface QuestionBankPopUpState {
-    show: boolean
     selected: string[]
 }
 
 class QuestionBankPopUp extends React.Component<QuestionBankPopUpProps, QuestionBankPopUpState> {
-    public state: QuestionBankPopUpState
-    constructor(props) {
-        super(props)
-        this.state = { show: props.show, selected: []}
+    public state: QuestionBankPopUpState = { 
+        selected: [] 
     }
-
-    public setSelected(id: string) {
+    
+    public toggleSelection(id: string) {
         let temp = this.state.selected
         if (!this.isInArray(id, temp)) {
             temp.push(id)
-            this.setState({ selected: temp })
         } else {
             temp.splice(temp.indexOf(id), 1)
-            this.setState({ selected: temp })
         }
+
+        this.setState({ selected: temp })
     }
 
-    private isInArray(value: string, array: string[]) {
-        return array.indexOf(value) > -1;
-    }
-
-    private listsubject(subjects: IQuestionBank[]) {
-        let select = this.state.selected
-        return (
-            subjects.map((subject, index) =>
-                <ButtonCool onClick={() => this.setSelected(subject.id!!)} selected={this.isInArray(subject.id!!, select)}>{subject.title}---{
-                    this.props.subjects[index].questionCount
-                }</ButtonCool>
-            )
-        )
-    }
-
-    private showArray=()=>{
-        
-        if (this.state.selected!=[]) {
-            let temp = this.state.selected
-            temp.sort()
-            this.setState({ selected: temp })
-            this.props.onClicked(this.state.selected)
-            
-        }
-        this.props.onClicked([])
-        this.setState({ selected: [] })
-    }
-
+    
     render() {
-        if (!this.state.show) {
-            return null;
-        }
         return (
             <Background>
                 <Popup>
                     {this.props.children}
                     <div className="footer">
                         <div className={css({ overflowY: 'scroll', height: 400 })}>
-                            <button onClick={this.props.onHide} className={css({ position: 'absolute', right: 10, top: 10 })}>Close</button>
-                            {this.listsubject(this.props.subjects)}
-                            <EditButton color={'green'} onClick={this.showArray}>Confirmar</EditButton>
+                            {this.props.subjects.map((subject, index) => (
+                                <ButtonCool 
+                                    onClick={() => this.toggleSelection(subject.id!!)} 
+                                    selected={this.isInArray(subject.id!!, this.state.selected)}>
+                                    {subject.title}---{this.props.subjects[index].questionCount}
+                                </ButtonCool>
+                            ))}
+                            <EditButton color={'green'} onClick={this.onConfirmClicked}>Confirmar</EditButton>
                         </div>
                     </div>
                 </Popup>
             </Background>
         );
     }
+
+    private isInArray = (value: string, array: string[]) => array.indexOf(value) > -1;
+    private onConfirmClicked = () => this.props.onSubmit(this.state.selected)
 }
 export default QuestionBankPopUp
--- src/ui/ExamMaker/index.tsx
+++ src/ui/ExamMaker/index.tsx
@@ -1,47 +1,108 @@
 import React, { Component } from 'react'
-import QuestionBankService from '../../services/QuestionCollectionService'
-import { IQuestion } from 'src/models/Question/IQuestion';
+import QuestionBankService from '../../services/QuestionBankService'
+import QuestionBankCollection from '../Exam/QuestionBankCollection';
+import { IQuestionBank } from 'src/models/QuestionBank/IQuestionBank';
+import { QuestionSelector } from 'src/services/QuestionSelector';
+import QuestionCollectionService from 'src/services/QuestionCollectionService';
 import ExamGenerator from '../ExamGenerator';
 import ExamRender from '../ExamGenerator/ExamRenderer';
+import { Title } from '../QuestionBankExplorer/Components';
 
 interface ExamMakerProps {
 }
 
 interface ExamMakerState {
-  selectedQuestionBanks: IQuestion[]
+  subjects: string[]
+  questionBankBySubject: Map<string, IQuestionBank[]>
+  questionCount: Map<string, number>
+  selectedSubject: string,
+  examCount: number,
 }
 // TODO: finsih
 class ExamMaker extends Component<ExamMakerProps, ExamMakerState> {
-  public state : ExamMakerState
-  private service = new QuestionBankService('RpWLrGWSsHFPHCF4OPDo')
+  public state : ExamMakerState = {
+    subjects: [],
+    questionBankBySubject: new Map(),
+    selectedSubject: '',
+    questionCount: new Map(),
+    examCount: 1
+  }
+
+  private service = new QuestionBankService()
+  private servic2 = new QuestionCollectionService('asdf')
+
 
   constructor(props) {
     super(props)
   
-    this.service.questions.subscribe(questions => {
-      this.setState({
-        ...this.state, 
-        selectedQuestionBanks: questions})
-    })
+    this.service.banks.subscribe(banks => { 
+      const subjects = Array.from(banks.keys())      
+      const newState: ExamMakerState = {
+        ...this.state,
+        subjects,
+        questionBankBySubject: banks
+      }
 
+      if (!this.state.selectedSubject) {
+        newState.selectedSubject = subjects[0]
+      }
+
+      this.setState(newState)
+    })
   }
 
   public render() {
     return (      
       <div>
-        <button onClick={this.todo}>boton</button>
-        <p>{JSON.stringify(this.state)}</p>
+        <Title>Generar Examen</Title>        
+        <h2>Materia del examen:</h2>
+        <select onChange={this.onSubjectSelected}>
+          {this.state.subjects.map(s =><option value={s}>{s}</option>)}
+        </select>
+        <h2>Selectionar temas</h2>
+        <QuestionBankCollection 
+          onChange={this.onCountChange}
+          questioncollections={this.subjects} />
+
+        <h2>Cuantos tipos de examenes se necesitan</h2>
+        <input placeholder="1" onChange={this.onExamCountChanged} value={this.state.examCount}></input>
+        <button onClick={this.generateExams}>Generar examenes</button>        
       </div>
     )
   }
 
-  private todo = () => {
-    const gen = new ExamGenerator(this.state.selectedQuestionBanks)
-    for (let i = 0; i < 3; i++) {
-      const exam = gen.generate()
-      const renderer = new ExamRender(exam)
-      renderer.render(`examen_tipo_${String.fromCharCode(97 + i)}`)
-    }    
+
+  private generateExams = () => {
+    const banks = Array.from(this.state.questionCount.keys())
+
+    this.servic2.getQuestionsForBank(banks).then((banks => {
+      const a = new QuestionSelector(banks, this.state.questionCount)
+      const questions = a.compute()
+      const gen = new ExamGenerator(questions)
+      
+      for (let i = 0; i < this.state.examCount; i++) {
+        const exam = gen.generate()
+        const renderer = new ExamRender(exam)
+        renderer.render(`examen_tipo_${String.fromCharCode(97 + i)}`)
+      }    
+    }))
+  }
+
+  private onSubjectSelected = (event) => {    
+    this.setState({ ...this.state, selectedSubject: event.target.value })
+  }
+
+  private onExamCountChanged = (event) => {
+    this.setState({ ...this.state, examCount: event.target.value })
+  }
+  
+  private onCountChange = (questionCount: Map<string, number>) => {
+    this.setState({ ...this.state, questionCount })
+  }
+
+  private get subjects() {
+    const { questionBankBySubject, selectedSubject } = this.state
+    return questionBankBySubject.get(selectedSubject) || []
   }
 }
 
