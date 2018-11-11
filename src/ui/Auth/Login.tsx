cc8d3c6236e166150ef17a82ea5fa0c6d507734a
paez74 <paez74@live.com.mx>
2018-11-11 16:45:23 -0600

    Included SignUp option

--- src/ui/Auth/Login.tsx
+++ src/ui/Auth/Login.tsx
@@ -16,25 +16,46 @@ class Login extends Component {
     this.authService.login(values.email, values.password)  
   }
 
-  // private signup = (values: FormValues) => {
-  //   this.authService.signup(values.email, values.password)  
-  // }
+   private signup = (values: FormValues) => {
+     this.authService.signup(values.email, values.password)  
+   }
 
   public render() {
     return (
-      <div> Bienvenido a Easy Exam porfavor inice sesion
+      <div>
+        <div> Bienvenido a Easy Exam <div>  Inicie Sesion </div></div>
+
       <Formik
         onSubmit={this.login}
         initialValues={{ email: '', password: '' }}
         render={
           () => (
             <Form>
+              <div>Usuario</div>
               <Field name="email" type="email" />
+              <div> Contraseña </div>
               <Field name="password" type="password" 
               />
               <button type="send">login</button>
             </Form>)
-      } />      
+      } />     
+
+       <div> <div></div></div>
+        <div>  Crear nueva cuenta </div>
+        <div><Formik
+        onSubmit={this.signup}
+        initialValues={{ email: 'E-Mail', password: '' }}
+        render={
+          () => (
+            <Form>
+              <div>Usuario</div>
+              <Field name="email" type="email" />
+              <div> Contraseña </div>
+              <Field name="password" type="password" 
+              />
+              <button type="send"> Sign Up </button>
+            </Form>)
+      } />  </div>
       </div>
       )
   }
